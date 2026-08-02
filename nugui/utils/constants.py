f47eb3ef4ff4bd1a8212959c6c2bf29bb7dc665c
Al Baeza <164692754+digitekxplorer@users.noreply.github.com>
2026-08-02 11:07:51 -0600

    Update constants.py
    See updates in main.py history section

--- nugui/utils/constants.py
+++ nugui/utils/constants.py
@@ -4,6 +4,7 @@ class ToolType(Enum):
     SELECT = 1
     STATE = 2
     LINE = 3
+    TEXT = 4  # FEATURE: free-floating text annotations
 
 # Geometry
 BUBBLE_RADIUS = 30
@@ -16,6 +17,7 @@ COLOR_STATE_FILL = "#f0f0f0"
 COLOR_LINE = "black"
 COLOR_SELECTED = "red"
 COLOR_TEMP_LINE = "gray"
+COLOR_ANNOTATION = "black"  # FEATURE: default annotation text color
 
 # Grid Settings
 GRID_SIZE = 20
